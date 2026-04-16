f0c31ed69ab5a57f66db1d594f9e07cc06285289
bposion0918 <b.posion.0918@gmail.com>
2026-04-16 16:11:08 +0900

    아이템 추가, 아이템 먹은 뒤 몇초 이후 효과 삭제, 함정 이미지 적용

--- GameEngine_MiddleProject_HJG/Assets/Script/PlayerController.cs
+++ GameEngine_MiddleProject_HJG/Assets/Script/PlayerController.cs
@@ -4,14 +4,15 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpforce = 5f;
+    public float moveSpeed = 2f;
+    public float jumpforce = 2f;
     public Transform groundCheck;
     public LayerMask groundLayer;
     public int maxJumpCount = 2;
 
     private Rigidbody2D rb;
     private bool isGrounded;
+    private bool Invincible = false;
     private float moveInput;
     private Animator pAni;
 
@@ -27,11 +28,22 @@ public class PlayerController : MonoBehaviour
         // 1. 이동 처리
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
-        // 2. 캐릭터 진행 방향에 따라 좌우 반전
-        if (moveInput > 0)
-            transform.localScale = new Vector3(1, 1, 1);
-        else if (moveInput < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
+
+        if(Invincible)
+        {
+            if (moveInput > 0)
+                transform.localScale = new Vector3(1, 1, 1);
+            else if (moveInput < 0)
+                transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            // 2. 캐릭터 진행 방향에 따라 좌우 반전
+            if (moveInput > 0)
+                transform.localScale = new Vector3(1, 1, 1);
+            else if (moveInput < 0)
+                transform.localScale = new Vector3(-1, 1, 1);
+        }
 
         // 3. 바닥 체크
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.05f, groundLayer);
@@ -77,9 +89,34 @@ public class PlayerController : MonoBehaviour
 
         if (collision.CompareTag("Enemy"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            if(Invincible)
+                Destroy(collision.gameObject);
+            else
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
-    }
 
+        if(collision.CompareTag("Invincible_Item"))
+        {
+            Invincible = true;
+            Invoke(nameof(ResetInvincible_Item), 5f);
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.CompareTag("Speed_Item"))
+        {
+            moveSpeed *= 1.5f;
+            Invoke(nameof(Speed_Item), 3f);
+            Destroy(collision.gameObject);
+        }
+
+    }
+        void ResetInvincible_Item()
+        {
+            Invincible = false;
+        }
+        void Speed_Item()
+        {
+            moveSpeed = 2f;
+        }
 }
 
